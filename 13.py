0badb5fefd9106154830cafbe95be94580f470fd
Abhinav Dash <131437239+Abhinav-Dash05@users.noreply.github.com>
2024-07-30 15:27:41 +0000

    modified:   13.py

--- 13.py
+++ 13.py
@@ -1,12 +1,12 @@
 import random as r
 import statistics as s
 
-A = r.randrange(1, 100, 16)
-B = r.randrange(1, 100, 16)
-C = r.randrange(1, 100, 16)
-D = r.randrange(1, 100, 16)
-E = r.randrange(1, 100, 16)
-F = r.randrange(1, 100, 16)
+A = r.randrange(1, 100, 10)
+B = r.randrange(1, 100, 10)
+C = r.randrange(1, 100, 10)
+D = r.randrange(1, 100, 10)
+E = r.randrange(1, 100, 10)
+F = r.randrange(1, 100, 10)
 
 a = (A,B,C,D,E,F)
 b = s.mean(a)
